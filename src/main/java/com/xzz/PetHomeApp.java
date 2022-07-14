86125cdc33014202a56e79f65b3f55564024f2f2
xzzhome <1091526909@qq.com>
2022-07-14 19:50:40 +0800

    这是第二次提交

--- src/main/java/com/xzz/PetHomeApp.java
+++ src/main/java/com/xzz/PetHomeApp.java
@@ -3,7 +3,7 @@ package com.xzz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//红红火火恍恍惚惚
+//呵呵哈哈哈
 @SpringBootApplication
 @MapperScan("com.xzz.*.mapper")//扫描多模块的mapper接口
 public class PetHomeApp {
