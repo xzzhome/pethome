28cf321026a9c3db5c29a44724fb6a4373270d80
xzzhome <108806808+xzzhome@users.noreply.github.com>
2022-07-21 12:40:12 +0800

    Update PetHomeApp.java

--- src/main/java/com/xzz/PetHomeApp.java
+++ src/main/java/com/xzz/PetHomeApp.java
@@ -3,7 +3,7 @@ package com.xzz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//233
 @SpringBootApplication
 @MapperScan("com.xzz.*.mapper")//扫描多模块的mapper接口
 public class PetHomeApp {
